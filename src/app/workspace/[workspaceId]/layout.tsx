99e69cd34e432527d60b2ce134d49156e6952759
hemel <smhemel.eu@gmail.com>
2025-05-18 01:13:50 +0600

    Implement functionality of workspace sidebar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -3,6 +3,8 @@
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
 import { Loader } from "lucide-react";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -15,6 +17,22 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20} defaultSize={80}>
+            {children}
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
