2ca262b33922492d614e078ca7da11f80f1217bb
hemel <smhemel.eu@gmail.com>
2025-05-17 12:23:58 +0600

    Implement functionality to retrieve user login data and display on user-button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { UserButton } from "@/features/auth/components/user-button";
 import { Loader } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="h-full flex items-center justify-center">
-      <Loader className="size-6 animate-spin text-muted-foreground" />
+      {/* <Loader className="size-6 animate-spin text-muted-foreground" /> */}
+      <UserButton />
     </div>
   );
 }
