748527c2e478f2f8d53dfaf93da7ff6073b2579e
hemel <smhemel.eu@gmail.com>
2025-05-17 15:29:03 +0600

    Create simple schema for workspaces and retrieve data functionality from database

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,9 +1,14 @@
 import { authTables } from "@convex-dev/auth/server";
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 const schema = defineSchema({
     ...authTables, // It will add some auth tables to our schema automatically
-    
+    workspaces: defineTable({
+        name: v.string(),
+        userId: v.id("users"),
+        joinCode: v.string(),
+    })
 });
 
 export default schema;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
-import { UserButton } from "@/features/auth/components/user-button";
+import { useEffect, useMemo } from "react";
 import { Loader } from "lucide-react";
+import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("Redirecting to workspace:", workspaceId);
+    } else if (!open) {
+      console.log('Open creation modal');
+    }
+  }, [workspaceId, isLoading]);
+
   return (
     <div className="h-full flex items-center justify-center">
       {/* <Loader className="size-6 animate-spin text-muted-foreground" /> */}
