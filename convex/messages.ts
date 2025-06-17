21b31c207dd6db7616fd50143b76134618abd34f
hemel <smhemel.eu@gmail.com>
2025-06-18 03:32:41 +0600

    Implement functionality to update and remove message from database.

--- convex/messages.ts
+++ convex/messages.ts
@@ -5,68 +5,6 @@ import { auth } from "./auth";
 import { Doc, Id } from "./_generated/dataModel";
 import { mutation, query, QueryCtx } from "./_generated/server";
 
-const getMember = async (
-  ctx: QueryCtx,
-  workspaceId: Id<"workspaces">,
-  userId: Id<"users">
-) => {
-  return ctx.db
-    .query("members")
-    .withIndex("by_workspace_id_user_id", (q) =>
-      q.eq("workspaceId", workspaceId).eq("userId", userId),
-    )
-    .unique();
-};
-
-export const create = mutation({
-  args: {
-    body: v.string(),
-    image: v.optional(v.id("_storage")),
-    workspaceId: v.id("workspaces"),
-    channelId: v.optional(v.id("channels")),
-    conversationId: v.optional(v.id("conversations")),
-    parentMessageId: v.optional(v.id("messages")),
-  },
-  handler: async (ctx, args) => {
-    const userId = await auth.getUserId(ctx);
-
-    if (!userId) {
-      throw new Error("Unauthorized");
-    }
-
-    const member = await getMember(ctx, args.workspaceId, userId);
-
-    if (!member) {
-      throw new Error("Unauthorized");
-    }
-
-    let _conversationId = args.conversationId;
-
-    // Only possible if we are replying in a thread in 1:1 conversation
-    if (!args.conversationId && !args.channelId && args.parentMessageId) {
-      const parentMessage = await ctx.db.get(args.parentMessageId);
-
-      if (!parentMessage) {
-        throw new Error("Parent message not found");
-      }
-
-      _conversationId = parentMessage.conversationId;
-    }
-
-    const messageId = await ctx.db.insert("messages", {
-      memberId: member._id,
-      body: args.body,
-      image: args.image,
-      channelId: args.channelId,
-      conversationId: _conversationId,
-      workspaceId: args.workspaceId,
-      parentMessageId: args.parentMessageId,
-    });
-
-    return messageId;
-  },
-});
-
 export const get = query({
   args: {
     channelId: v.optional(v.id("channels")),
@@ -254,6 +192,130 @@ export const getById = query({
       member,
       reactions: reactionsWithoutMemberIdProperty,
     };
+  },
+});
+
+const getMember = async (
+  ctx: QueryCtx,
+  workspaceId: Id<"workspaces">,
+  userId: Id<"users">
+) => {
+  return ctx.db
+    .query("members")
+    .withIndex("by_workspace_id_user_id", (q) =>
+      q.eq("workspaceId", workspaceId).eq("userId", userId),
+    )
+    .unique();
+};
+
+export const create = mutation({
+  args: {
+    body: v.string(),
+    image: v.optional(v.id("_storage")),
+    workspaceId: v.id("workspaces"),
+    channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
+    parentMessageId: v.optional(v.id("messages")),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await getMember(ctx, args.workspaceId, userId);
+
+    if (!member) {
+      throw new Error("Unauthorized");
+    }
+
+    let _conversationId = args.conversationId;
+
+    // Only possible if we are replying in a thread in 1:1 conversation
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
+      }
+
+      _conversationId = parentMessage.conversationId;
+    }
+
+    const messageId = await ctx.db.insert("messages", {
+      memberId: member._id,
+      body: args.body,
+      image: args.image,
+      channelId: args.channelId,
+      conversationId: _conversationId,
+      workspaceId: args.workspaceId,
+      parentMessageId: args.parentMessageId,
+    });
+
+    return messageId;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("messages"),
+    body: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const message = await ctx.db.get(args.id);
+
+    if (!message) {
+      throw new Error("Message not found");
+    }
+
+    const member = await getMember(ctx, message.workspaceId, userId);
+
+    if (!member || member._id !== message.memberId) {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.patch(args.id, {
+      body: args.body,
+      updatedAt: Date.now(),
+    });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id("messages"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const message = await ctx.db.get(args.id);
+
+    if (!message) {
+      throw new Error("Message not found");
+    }
+
+    const member = await getMember(ctx, message.workspaceId, userId);
+
+    if (!member || member._id !== message.memberId) {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
   },
 });
 
